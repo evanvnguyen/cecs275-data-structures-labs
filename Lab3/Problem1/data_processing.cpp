c7f4ebeb75635935d476dfde284c91b109214449
Evan Nguyen <59784844+evanvnguyen@users.noreply.github.com>
2022-03-05 01:19:44 -0800

    added swap function

--- Lab3/Problem1/data_processing.cpp
+++ Lab3/Problem1/data_processing.cpp
@@ -11,6 +11,7 @@ using namespace std;
 // float avg - takes a vector<int> as an argument and returns the average
 // vector<int> frequency - takes a sorted vector<int> as an argument and returns a vector that contains the count of a number connected via subscript (i.e., #1 = vector[0], where vector[0] contains 3. #1's frequency is 3.). parallel array
 // void output_file - takes frequency vector<int>, min, max, and avg as an argument and outputs formatted data into frequency.txt
+// void swap - takes reference arguments a & b and swaps them. use for sorting
 
 vector<int> read_data(string inputFile);
 vector<int> sort_data(vector<int> rawData);
@@ -18,9 +19,10 @@ vector<int> frequency(vector<int> sortedData);
 
 int max_val(vector<int> sortedData);
 int min_val(vector<int> sortedData);
-
 float avg(vector<int> sortedData);
+
 void output_file(vector<int> frequencyData, int maxValue, int minValue, float avgValue);
+void swap(int &a, int&b);
 
 int main(){
     string file = "data.txt";
@@ -39,6 +41,12 @@ int main(){
     //DEBUG
     //read_data(file);
     return 0;
+}
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 vector<int> read_data(string inputFile) {

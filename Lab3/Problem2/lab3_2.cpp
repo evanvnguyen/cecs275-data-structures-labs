99f4a4a027dc45469700951ebb3a1e4e1a79840c
Evan Nguyen <59784844+evanvnguyen@users.noreply.github.com>
2022-03-05 02:30:52 -0800

    Add files via upload

---
+++ Lab3/Problem2/lab3_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void reverse(char s[]);
+void concat(const char  a[], const char b[]), char result[], int result_maxlength);
+
+int main(){
+
+    return 0;
+}
+
+void reverse(char s[]){
+
+}
+
+void concat(const char  a[], const char b[]), char result[], int result_maxlength){
+    
+}

3f53febd1d4a9b6ecc75cdc64b0200693400379f
Jusheph <jaygee1019@gmail.com>
2022-03-11 19:56:28 -0800

    Added Read_Data and Frequency

--- Lab3/Problem1/data_processing.cpp
+++ Lab3/Problem1/data_processing.cpp
@@ -18,11 +18,9 @@ using namespace std;
 vector<int> read_data(string inputFile);
 vector<int> sort_data(vector<int> rawData, int vecSize);
 vector<int> frequency(vector<int> sortedData);
-
 int max_val(vector<int> sortedData);
 int min_val(vector<int> sortedData);
 float avg(vector<int> sortedData, int vecSize);
-
 void output_file(vector<int> frequencyData, int maxValue, int minValue, float avgValue);
 void swap(int &a, int&b);
 
@@ -30,7 +28,7 @@ int main(){
     
     string file = "data.txt";
     int vecSize = 0;
-
+    
     vector<int> rawData = read_data(file);
 
     //TEST VECTOR
@@ -42,8 +40,8 @@ int main(){
     }
 
     vector<int> sortedData = sort_data(rawData, vecSize);
-    //vector<int> frequencyData = frequency(sortedData);
-
+    vector<int> frequencyData = frequency(sortedData);
+    
     int minValue = min_val(sortedData);
     int maxValue = max_val(sortedData);
     
@@ -62,40 +60,66 @@ void swap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
-
-}
-void convert(char num_Temp){
-    char d = num_Temp;
-    string y = "";
-    y += d;
-    int num_Converted;
-    num_Converted = stoi(y);
-    cout << num_Converted;
 }
 
 vector<int> read_data(string inputFile) {
     string word;
+    string temp_Word;
+    int temp_Num;
+    vector<int> number_List;
+    int int1;
+    int newInt;
+    int store;
+    int temp = 0;
     ifstream inFile;
     inFile.open(inputFile);
-    if (inFile){
-        cout << "Open success!" << endl;
+    if (!inFile){
+        cout << "File Not Found!" << endl;
+    }
+    else{
+        cout << "File Found!" << endl;
     }
     //int i = 0;
     while(inFile >> word){
-        for (int j = 0; j < word.length(); j++){
-            if (word[j]==','){
-                word.erase(j);
-                cout << " ";
+        //For loop that removes commas from the string
+        for (int i = 0; i < word.length(); i++){
+            if (word[i] == ',')
+            {
+                word[i] = ' ';
             }
-            else{
-            convert(word[j]);
-            }
-
-        //temp.push_back(word);
-        //cout << temp[i] << endl;
-        //i++;
+            // Debug
+            temp_Word += word[i]; // temp_Word holds our number list with spaces instead of commas
         }
     }
+
+    cout << temp_Word;
+
+
+    for (int i = 0; i < temp_Word.length(); i++){
+        if (temp_Word[i] != ' ') { // ignore space
+            int1 = (temp_Word[i] - '0') % 10; 
+            temp *= 10;
+            newInt = int1 + temp;
+            //cout << "newInt: " << newInt << endl;
+            temp = int1;
+        } 
+        else {
+            store = newInt;
+            number_List.push_back(store);
+            //cout << "store: " << store << endl;
+            int1 = 0;
+            temp = 0;
+        }
+    }
+    store = newInt;
+    number_List.push_back(store);
+
+    //cout << "store at end: " << store << endl;
+    
+    for (int i : number_List){
+        cout << i << " ";
+    }
+    return number_List;
 }
 
 vector<int> sort_data(vector<int> rawData, int vecSize){
@@ -115,9 +139,11 @@ vector<int> sort_data(vector<int> rawData, int vecSize){
     }  
 
     // check if array sorted
+    cout << endl << endl << "Sorted from Min to Max" << endl;   
     for (int j : rawData){
-        cout << j << endl;
+        cout << j << " ";
     }
+    cout << endl;
 
     return rawData;
 }
@@ -125,7 +151,7 @@ vector<int> sort_data(vector<int> rawData, int vecSize){
 // min value is the first element in a sorted vector
 int min_val(vector<int> sortedData){
 
-    cout << "Sorted Data Min Value: " << sortedData[0] << endl;
+    cout << "\nSorted Data Min Value: " << sortedData[0] << endl;
 
     return sortedData[0];
 }
@@ -133,7 +159,7 @@ int min_val(vector<int> sortedData){
 // max value is the last element in a sorted vector
 int max_val(vector<int> sortedData){
 
-    cout << "Sorted Data Max Value: " << sortedData[sortedData.size()-1] << endl;
+    cout << "\nSorted Data Max Value: " << sortedData[sortedData.size()-1] << endl;
 
     return sortedData[sortedData.size()-1];
 }
@@ -150,6 +176,43 @@ float avg(vector<int> sortedData, int vecSize){
 
     // round to 2 decimal points
     average_2pt = ceil((sum / vecSize) * 100.0) / 100.0;
-    //cout << "avg " << average_2pt << endl;
+    //cout << "\n" << "The Average is " << average_2pt << endl;
     return average_2pt;
 }
+
+vector<int>frequency(vector<int> sortedData){
+    vector<int>count;
+    int value;
+    int freq = 0;
+    int unique_Val;
+    int range = sortedData.size();
+    char asterisks = '*';
+    cout << '\n';
+    for (int i = 0; i < range; i++){
+        unique_Val = sortedData[i];
+        //count.push_back(unique_Val);
+        int j = 0;
+        cout << unique_Val;
+        while (sortedData[i+j]==unique_Val){
+            //cout << "sortedData[" << i << "+" << j << "]: " << sortedData[i+j] << " unique_Val: " << unique_Val << endl;
+            j++;
+            freq++;
+            //cout << "Unique_Val: " << unique_Val << "Frequency: " << freq << endl;
+        }
+        cout << ": ";
+        for (int i=0; i < freq; i++){
+            if (freq != 0){
+                cout << "*";
+            }
+            else{
+                break;
+            }
+        }
+        cout << " " << "(" << freq << ")" << endl;
+        if (sortedData[i+j]!= unique_Val){
+            i = i+j-1;
+        }
+        freq = 0;
+    }
+}
+   
